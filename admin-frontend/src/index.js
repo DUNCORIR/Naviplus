f9e2bf0ab43c6a5d4816867c46647fd9dcb4e431
duncorir <duncorir@mail.com>
2025-07-02 15:06:13 +0300

    Add global CSS for base styling

--- admin-frontend/src/index.js
+++ admin-frontend/src/index.js
@@ -6,6 +6,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/global.css';
+
 
 // Mount the App component into root div
 const root = ReactDOM.createRoot(document.getElementById('root'));
