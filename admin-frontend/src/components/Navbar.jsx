a68d44567ce662de9d0648a07f310317013c6749
duncorir <duncorir@mail.com>
2025-07-03 18:10:52 +0300

    Fix navbar  rendering and increase Naviplus logo visibility in navbar

--- admin-frontend/src/components/Navbar.jsx
+++ admin-frontend/src/components/Navbar.jsx
@@ -11,24 +11,22 @@ function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false); // State to control mobile menu
 
   // Toggle menu visibility on mobile
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   // Close menu when a link is clicked
-  const closeMenu = () => {
-    setMenuOpen(false);
-  };
+  const closeMenu = () => setMenuOpen(false);
 
   return (
     <nav className="navbar">
-      {/* Brand: logo + text */}
+      {/* Brand: logo + title */}
       <div className="navbar-brand">
-        <img
-          src="/images/naviplus-logo.png"
-          alt="Naviplus Logo"
-          className="navbar-logo"
-        />
+        <div className="navbar-logo-wrapper">
+          <img
+            src="/images/naviplus-logo.png"
+            alt="Naviplus Logo"
+            className="navbar-logo"
+          />
+        </div>
         <Link to="/" className="navbar-link brand-link">Naviplus</Link>
       </div>
 
@@ -42,10 +40,10 @@ function Navbar() {
       {/* Main navigation links */}
       <div className={`navbar-links ${menuOpen ? 'show' : ''}`}>
         <Link to="/" className="navbar-link" onClick={closeMenu}>Home</Link>
-        <Link to="/signup" className="navbar-link" onClick={closeMenu}>Sign Up</Link>
         <Link to="/login" className="navbar-link" onClick={closeMenu}>Login</Link>
         <Link to="/buildings" className="navbar-link" onClick={closeMenu}>Manage Buildings</Link>
         <Link to="/add-building" className="navbar-link" onClick={closeMenu}>Add Building</Link>
+        <Link to="/signup" className="navbar-link" onClick={closeMenu}>Sign Up</Link>
       </div>
     </nav>
   );
