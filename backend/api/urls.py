21e808f8024ea459a7e821f23c9187ac84759d4c
duncorir <duncorir@mail.com>
2025-07-04 11:49:19 +0300

    backend APIs to match the updated frontend behavior

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -14,6 +14,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BuildingViewSet, PLDViewSet, UserProfileViewSet
 from .views import signup
+from rest_framework.authtoken import views as auth_views
 
 # Initialize default router for ViewSets
 router = DefaultRouter()
@@ -25,4 +26,5 @@ router.register(r'user-profiles', UserProfileViewSet, basename='userprofile')
 urlpatterns = [
     path('', include(router.urls)),
     path('signup/', signup, name='signup'),
+    path('login/', auth_views.obtain_auth_token, name='login'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -18,7 +18,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 class BuildingViewSet(viewsets.ModelViewSet):
     """
